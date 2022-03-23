99a02d4c06008198e7be70b54f54c20694f4ce92
Ankit Jain <ankitjain5019@gmail.com>
2022-03-23 12:17:11 -0400

    Added Mongoose lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './configuration/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const MONGODB_URI = 'mongo.uri';
 
 @Module({
   imports: [
@@ -10,8 +13,19 @@ import configuration from './configuration/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>(MONGODB_URI),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number;
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = this.configService.get<number>('port');
+  }
+}
