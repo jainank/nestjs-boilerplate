552bb71c233a1620c9bc8c583673400ddaf6ec36
Ankit Jain <ankitjain5019@gmail.com>
2022-03-23 16:50:49 -0400

    Added helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './configuration/swagger-steup';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   setupSwagger(app);
+  app.use(helmet());
   await app.listen(AppModule.port);
 }
 bootstrap();
